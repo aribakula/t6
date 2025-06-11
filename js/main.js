392c527e137a1aa4d3df7c5861963bad712a4030
Ari <arba0001@stud.kea.dk>
2025-06-11 09:38:21 +0200

    Tilføjer alt

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const openBtn1 = document.querySelector("#openBtn1");
+openBtn1.addEventListener("click", openDialog);
+function openDialog() {
+  document.querySelector("#dialog-1").showModal();
+}
